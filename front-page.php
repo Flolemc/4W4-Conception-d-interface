7ad1055402acbed595e859fe373d3a5dee1ced19
Florence Lemieux Cayer <flolemc@gmail.com>
2022-02-25 15:01:37 -0500

    Ajout de la barre de recherche

--- front-page.php
+++ front-page.php
@@ -9,12 +9,32 @@
     <div class="animation__bloc">5</div>
 </section>
 
-    <h1>---- Front-page.php ------</h1>
-   <?php if (have_posts()): the_post(); ?>
-        <?php the_title() ?>
-        <?php the_content() ?>   
+    <h1>Page d'accueil</h1>
+
+    <h2>Nouveaux cours :</h2>
+
+    <div class="nouveau__cours">
+        <?php if (have_posts()): the_post(); ?>
+            <?php
+                $titre = get_the_title();
+                $titreFiltreCours = substr($titre, 7, -6);
+                $nbHeures = substr($titre, -6);
+                $sigleCours = substr($titre, 0, 7);
+                $descCours = get_the_content();
+                ?>
+            
+            <?php the_post_thumbnail("thumbnail"); ?>
+                <h3 class="cours__titre">
+                    <a href="<?php echo get_permalink(); ?>">
+                        <?= $titreFiltreCours; ?>
+                    </a>
+                </h3>
+                <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
+                <p class="cours__sigle"><?= $sigleCours; ?> </p>
+                <p class="cours__desc"> <?= $descCours; ?></p>  
   
-   <?php endif ?>
+        <?php endif ?>
+    </div>
    
 </main>
 <?php get_footer() ?>
