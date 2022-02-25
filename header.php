fbad00142438f18fba7b41461b8f5e98ad9e6bbf
Florence Lemieux Cayer <flolemc@gmail.com>
2022-02-25 14:24:25 -0500

    commit lol

---
+++ header.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * L'entête « header » de chacune des pages de notre site
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <?php wp_head() ?>
+    <?php show_admin_bar(true); ?>
+</head>
+<body  <?php body_class("site"); ?>>
+<header class="site__header">
+    <div class="header__texte">
+    <h1 class="header__titre">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">    
+            <?php bloginfo('name'); ?></h1>
+        </a>
+    <h2 class="header__description"><?php bloginfo('description'); ?></h2>
+    </div>
+    <section class="util">
+        <div class="util__lien">
+            <form action="<?php echo get_home_url('/') ?>" class="recherche">
+                <input type="text" class="recherche__input" value="<?php echo get_search_query() ?>">
+                <button class="recherche__bouton">
+                <svg width="15px" height="15px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="black"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                </button>
+            </form>
+        </div>
+    </section>
+</header>
+<section class="site__barre">
+    <input type="checkbox"  id="chk-burger">
+    <label for="chk-burger" id="burger">
+        <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#ddd"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+    </label>
+    <?php wp_nav_menu(array("menu"=>"principal",
+                            "container"=>"nav")); ?>
+</section>
